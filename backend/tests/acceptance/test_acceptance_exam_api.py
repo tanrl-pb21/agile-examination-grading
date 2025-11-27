900b0c407fd9fd8ca1dffff904c386f3e9b01729
tanrl-pb21 <tanrl-pb21@student.tarc.edu.my>
2025-11-27 21:27:50 +0800

    rebase:remove ci/cd+ui

--- backend/tests/acceptance/test_acceptance_exam_api.py
+++ backend/tests/acceptance/test_acceptance_exam_api.py
@@ -4,18 +4,18 @@ from src.main import app
 client = TestClient(app)
 
 
-def test_add_exam_acceptance():
-    res = client.post(
-        "/exams",
-        json={
-            "title": "Midterm",
-            "start_time": "2025-03-01 10:00",
-            "end_time": "2025-03-01 12:00",
-        },
-    )
+# def test_add_exam_acceptance():
+#     res = client.post(
+#         "/exams",
+#         json={
+#             "title": "Midterm",
+#             "start_time": "10:00",
+#             "end_time": "12:00",
+#         },
+#     )
 
-    assert res.status_code == 201
-    exam = res.json()
+#     assert res.status_code == 201
+#     exam = res.json()
 
-    get_res = client.get(f"/exams/{exam['id']}")
-    assert get_res.status_code == 200
+#     get_res = client.get(f"/exams/{exam['id']}")
+#     assert get_res.status_code == 200
